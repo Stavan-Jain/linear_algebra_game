fdb8e8ee56676f55d4755e8f3d04f1438a36e584
MithicSpirit <rpc01234@gmail.com>
2023-06-08 14:31:46 -0400

    Change tuple vectors to real numbers
    Now they're actual vectors :)
    
    repr still uses cauchy sequences (because that's what the repr for real
    numbers uses) but that's fine for the time being.

--- src/vectors/tuple.lean
+++ src/vectors/tuple.lean
@@ -1,29 +1,28 @@
 import data.real.basic
-import data.int.basic
 
 
 inductive tuple : ℕ → Type
 | nil : tuple 0
-| cons {n : ℕ} (car : ℤ) (cdr : tuple n) : (tuple (n + 1))
+| cons {n : ℕ} (car : ℝ) (cdr : tuple n) : (tuple (n + 1))
 
 notation `[[` l:(foldr `, ` (h t, tuple.cons h t) tuple.nil `]]`) := l
 
 
-protected def tuple.repr : ∀ {n : ℕ}, tuple n → string
+protected meta def tuple.repr : ∀ {n : ℕ}, tuple n → string
 | 0 _ := "tuple.nil"
-| _ (tuple.cons a b) := "(tuple.cons " ++ has_repr.repr a ++ " " ++ tuple.repr b ++ ")"
+| n (tuple.cons a b) := "(tuple.cons " ++ repr a ++ " " ++ tuple.repr b ++ ")"
 
-instance (n : ℕ) : has_repr (tuple n) := ⟨tuple.repr⟩
+meta instance (n : ℕ) : has_repr (tuple n) := ⟨tuple.repr⟩
 
 
 protected def tuple.add : ∀ {n : ℕ}, tuple n → tuple n → tuple n
 | 0 _ _ := tuple.nil
-| n (tuple.cons head₁ tail₁) (tuple.cons head₂ tail₂) := tuple.cons (head₁ + head₂) (tuple.add tail₁ tail₂)
+| _ (tuple.cons head₁ tail₁) (tuple.cons head₂ tail₂) := tuple.cons (head₁ + head₂) (tuple.add tail₁ tail₂)
 
 instance (n : ℕ) : has_add (tuple n) := ⟨tuple.add⟩
 
 
-def tuple.dot_product : ∀ {n : ℕ}, tuple n → tuple n → ℤ
+def tuple.dot_product : ∀ {n : ℕ}, tuple n → tuple n → ℝ
 | 0 _ _ := 0
 | _ (tuple.cons head₁ tail₁) (tuple.cons head₂ tail₂) := (head₁ * head₂) + tuple.dot_product tail₁ tail₂
 
@@ -36,11 +35,11 @@ def tuple.cross_product : tuple 3 → tuple 3 → tuple 3
 infixl ` ×ᵥ `:74 := tuple.cross_product
 
 
-def tuple.scalar_mul: ∀ {n : ℕ}, ℤ → tuple n → tuple n
+def tuple.scalar_mul: ∀ {n : ℕ}, ℝ → tuple n → tuple n
 | 0 _ _ := [[]]
-| n a (tuple.cons head tail) := tuple.cons (a*head) (tuple.scalar_mul a tail)
+| _ a (tuple.cons head tail) := tuple.cons (a*head) (tuple.scalar_mul a tail)
 
 infix ` ** `:69 := tuple.scalar_mul
 
 
-def tuple.norm_sq {n : ℕ} (v : tuple n) : ℤ  := v ⬝ v
+def tuple.norm_sq {n : ℕ} (v : tuple n) : ℝ  := v ⬝ v
