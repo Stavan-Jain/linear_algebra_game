2cdf6a2c529ccdf6955eaf3cd64479a64d175730
MithicSpirit <rpc01234@gmail.com>
2023-06-08 13:59:59 -0400

    Change notations to infixes
    Needed to update lean and mathlib, though this also probably fixes the
    issue with representing real numbers. Also cleans up formatting.

--- src/vectors/tuple.lean
+++ src/vectors/tuple.lean
@@ -1,5 +1,4 @@
 import data.real.basic
-import tactic.norm_num
 import data.int.basic
 
 
@@ -7,54 +6,41 @@ inductive tuple : ℕ → Type
 | nil : tuple 0
 | cons {n : ℕ} (car : ℤ) (cdr : tuple n) : (tuple (n + 1))
 
-
 notation `[[` l:(foldr `, ` (h t, tuple.cons h t) tuple.nil `]]`) := l
 
 
 protected def tuple.repr : ∀ {n : ℕ}, tuple n → string
 | 0 _ := "tuple.nil"
 | _ (tuple.cons a b) := "(tuple.cons " ++ has_repr.repr a ++ " " ++ tuple.repr b ++ ")"
+
 instance (n : ℕ) : has_repr (tuple n) := ⟨tuple.repr⟩
 
 
 protected def tuple.add : ∀ {n : ℕ}, tuple n → tuple n → tuple n
 | 0 _ _ := tuple.nil
 | n (tuple.cons head₁ tail₁) (tuple.cons head₂ tail₂) := tuple.cons (head₁ + head₂) (tuple.add tail₁ tail₂)
+
 instance (n : ℕ) : has_add (tuple n) := ⟨tuple.add⟩
 
 
-
-def tuple.dotproduct : ∀ {n : ℕ}, tuple n → tuple n → ℤ  
+def tuple.dot_product : ∀ {n : ℕ}, tuple n → tuple n → ℤ
 | 0 _ _ := 0
-| _ (tuple.cons head₁ tail₁) (tuple.cons head₂ tail₂) := (head₁ * head₂) + tuple.dotproduct tail₁ tail₂
+| _ (tuple.cons head₁ tail₁) (tuple.cons head₂ tail₂) := (head₁ * head₂) + tuple.dot_product tail₁ tail₂
 
-notation  a ⬝ b := tuple.dotproduct a b 
+infix ` ⬝ `:72 := tuple.dot_product
 
-def tuple.crossproduct : tuple 3 → tuple 3 → tuple 3
+
+def tuple.cross_product : tuple 3 → tuple 3 → tuple 3
 | [[a, b, c]] [[d, e, f]] := [[b * f - c * e, c * d - a * f, a * e - b * d]]
 
-notation a × b := tuple.crossproduct a b
+infixl ` ×ᵥ `:74 := tuple.cross_product
 
-def tuple.normsq {n : ℕ} (v : tuple n) : ℤ   :=  tuple.dotproduct v v
-
---def tuple.norm {n : ℕ} (v : tuple n) : ℝ  :=  (nat.sqrt (int.to_nat (tuple.normsq v)))
 
 def tuple.scalar_mul: ∀ {n : ℕ}, ℤ → tuple n → tuple n
 | 0 _ _ := [[]]
 | n a (tuple.cons head tail) := tuple.cons (a*head) (tuple.scalar_mul a tail)
 
-notation a `**` b := tuple.scalar_mul a b
-
-namespace tuple 
-#eval normsq [[1, 2, 3]]
-#eval [[3 , 4 , 5]] + [[3, 6, 7]]
-#eval crossproduct [[12, 10, 5]] [[13, 14, 17]]
-#eval scalar_mul 4 [[1, 2, 3]]
-#eval [[1,2]] ⬝ [[2, 3]]  
-#eval [[12, 10, 5]] × [[13, 14, 17]]
-#eval 4 ** [[1, 2, 5]]
-
-end tuple
+infix ` ** `:69 := tuple.scalar_mul
 
 
-#eval (3:ℝ)
+def tuple.norm_sq {n : ℕ} (v : tuple n) : ℤ  := v ⬝ v
