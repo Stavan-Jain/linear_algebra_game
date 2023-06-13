a28a32881aaf52ddc3ac0a6f9d67fc6c34586c61
MithicSpirit <rpc01234@gmail.com>
2023-06-13 15:11:17 -0400

    Reorder definitions
    Prevents splitting up definition about norm.

--- src/vectors/tuple.lean
+++ src/vectors/tuple.lean
@@ -73,14 +73,15 @@ def norm_sq {n : ℕ} (v : tuple n) : nnreal := ⟨v ⬝ v, begin
     exact add_nonneg this hn, },
 end⟩
 
-def nth : ∀ {n : ℕ} (i : ℕ), tuple n → i < n → ℝ 
-| 0 i _ prf := absurd prf i.not_lt_zero
-| _ 0 (cons head _) prf := head
-| _ (i+1) (cons _ tail) prf := nth i (tail) (nat.le_of_succ_le_succ prf)
-
 protected noncomputable def norm {n : ℕ} (v : tuple n) : nnreal := nnreal.sqrt (norm_sq v)
 noncomputable instance {n : ℕ} : has_norm (tuple n) := ⟨coe ∘ tuple.norm⟩
 noncomputable instance {n : ℕ} : has_nnnorm (tuple n) := ⟨tuple.norm⟩
+
+
+def nth : ∀ {n : ℕ} (i : ℕ), tuple n → i < n → ℝ
+| 0 i _ prf := absurd prf i.not_lt_zero
+| _ 0 (cons head _) prf := head
+| _ (i+1) (cons _ tail) prf := nth i (tail) (nat.le_of_succ_le_succ prf)
 
 
 protected def zero : ∀ {n : ℕ}, tuple n
