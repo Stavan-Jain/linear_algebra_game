41e1a86a788b340eb2f57ccce62bd6ff570744f2
Stavan Jain <stavansjain@gmail.com>
2023-06-13 15:04:06 -0400

    working versions of level 1 and 2

--- src/game/vector_world/level2.lean
+++ src/game/vector_world/level2.lean
@@ -1,4 +1,5 @@
 import vectors.tuple -- hide
+import data.real.basic
 namespace tuple -- hide
 
 /- 
@@ -13,10 +14,14 @@ We're going to prove that { [0, 1] , [1, 0] } forms a basis for ℝ²
 /- Lemma
 And vector in ℝ² can be expressed as a linear combination of the vectors [0, 1] and [1, 0]
 -/
-
-lemma lin_comb: ∀  (a: tuple 2), ∃(d₁ : ℤ)(d₂ : ℤ) , a = (d₁ ** [[0, 1]]) + (d₂** [[1, 0]]) :=
+lemma lin_comb: ∀  (i: ℝ) (j :ℝ ), ∃(d₁ : ℝ )(d₂ : ℝ ) , [[i, j]] =   (d₁ ** [[1, 0]]) + (d₂** [[0, 1]]) :=
 begin 
- sorry, 
+  intros i j ,
+  use [i, j], 
+  dsimp [scalar_mul, map], 
+  simp, 
+  simp [has_add.add], dsimp [tuple.add], 
+  simp, 
 end
 
 end tuple -- hide
