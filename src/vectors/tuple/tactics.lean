5661ecd531ad60f0ffa66d5e3e9adda459dee780
MithicSpirit <rpc01234@gmail.com>
2023-06-14 15:56:46 -0400

    Simplify add_cons_eq_cons_add proof
    Only one simp statement is needed. The reason we need this instead of
    just marking `has_add.add` and `tuple.add` as `[simp]` is that otherwise
    under normal circumstances `simp` is too eager: it expands `has_add.add`
    for real numbers as well as for tuples, and it seems like most/all
    proofs about addition on real numbers are on `has_add.add` rather than
    normal addition. This makes it pretty much impossible to progress.

--- src/vectors/tuple/tactics.lean
+++ src/vectors/tuple/tactics.lean
@@ -5,9 +5,7 @@ open tuple
 @[simp]
 lemma add_cons_eq_cons_add {n : ℕ} (u₁ v₁ : ℝ) (uₙ vₙ : tuple n)
   : (cons u₁ uₙ) + (cons v₁ vₙ) = cons (u₁ + v₁) (uₙ + vₙ) := begin
-  simp [has_add.add],
-  simp [tuple.add],
-  refl,
+  simp [has_add.add, tuple.add],
 end
 
 @[simp]
