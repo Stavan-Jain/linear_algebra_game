0975f4a4a36f83c9d8090ea5ffa6240cf4aa2ac0
Stavan Jain <stavansjain@gmail.com>
2023-06-14 14:36:14 -0400

    completed proof for level 8

--- src/game/vector_world/level8.lean
+++ src/game/vector_world/level8.lean
@@ -1,5 +1,8 @@
 import vectors.tuple -- hide
 import data.real.basic
+import game.vector_world.level7-- hide 
+import game.vector_world.level3-- hide
+
 namespace tuple -- hide
 
 /- 
@@ -14,9 +17,17 @@ namespace tuple -- hide
 -/
 
 lemma add_norm_square: ∀ {n : ℕ} (x: tuple n) (y : tuple n) 
-,  norm_sq (x + y) = norm_sq x + (2 * (x ⬝ y)) + norm_sq y :=
+,  ↑(norm_sq (x + y)) = ↑(norm_sq x) + (2 * (x ⬝ y)) + ↑(norm_sq y) :=
 begin 
-  sorry, 
+  intros n x y, 
+  dsimp [norm_sq],  
+  rw dot_dist, 
+  rw dot_comm, 
+  rw dot_dist, 
+  nth_rewrite 2 dot_comm, 
+  rw dot_dist, 
+  nth_rewrite 2 dot_comm,
+  linarith,  
 end
 
 end tuple -- hide
