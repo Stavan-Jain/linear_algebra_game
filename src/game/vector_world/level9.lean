b9333dfe620e566df5deab1af0ea2fb0fa4dc3d5
Stavan Jain <stavansjain@gmail.com>
2023-06-14 14:59:29 -0400

    added level 10 sketch

--- src/game/vector_world/level9.lean
+++ src/game/vector_world/level9.lean
@@ -1,21 +1,35 @@
 import vectors.tuple -- hide
 import data.real.basic
+import game.vector_world.level7-- hide 
+import game.vector_world.level3-- hide
+import game.vector_world.level8 --hide
+
 namespace tuple -- hide
 
 /- 
 # Vector world
 
-## Level 9: `Cauchy-Schwarz Inequality` 
+## Level 8: `Add norm square` 
 
 -/
 
 /- Lemma
-|x · y| ≤ ||x|| * ||y||.-/
+||x + y||² = ||x||² + 2 * (x ⬝ y) + ||y||²
+-/
 
-lemma cauchy_schwarz: ∀ {n : ℕ} (x: tuple n) (y : tuple n) 
-, | x ⬝ y| ≤ norm_sq x * norm_sq y :=
+
+lemma sub_norm_square: ∀ {n : ℕ} (x: tuple n) (y : tuple n) 
+,  ↑(norm_sq (x - y)) = ↑(norm_sq x) - (2 * (x ⬝ y)) + ↑(norm_sq y) :=
 begin 
-  sorry, 
+  intros n x y, 
+  dsimp [norm_sq],  
+  rw dot_dist, 
+  rw dot_comm, 
+  rw dot_dist, 
+  nth_rewrite 2 dot_comm, 
+  rw dot_dist, 
+  nth_rewrite 2 dot_comm,
+  linarith,  
 end
 
 end tuple -- hide
