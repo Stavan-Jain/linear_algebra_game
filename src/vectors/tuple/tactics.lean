faf79010160ed00ed5df89aedc36e3a286d51849
MithicSpirit <rpc01234@gmail.com>
2023-06-15 14:40:13 -0400

    Improve/add more [simp] lemmas
    Their proofs are changed to just `rfl`, as it evidences that these are
    incredibly basic things that are fine to provide to the students as-is.
    
    This also adds sub_cons_eq_cons_sub, neg_cons_eq_cons_neg, nil_add_nil,
    and zero_cons.

--- src/vectors/tuple/tactics.lean
+++ src/vectors/tuple/tactics.lean
@@ -1,15 +1,29 @@
 import vectors.tuple
 
-open tuple
+namespace tuple
+
 
 @[simp]
 lemma add_cons_eq_cons_add {n : ℕ} (u₁ v₁ : ℝ) (uₙ vₙ : tuple n)
-  : (cons u₁ uₙ) + (cons v₁ vₙ) = cons (u₁ + v₁) (uₙ + vₙ) := begin
-  simp [has_add.add, tuple.add],
-end
+  : (cons u₁ uₙ) + (cons v₁ vₙ) = cons (u₁ + v₁) (uₙ + vₙ) := rfl
 
 @[simp]
-lemma mul_cons_eq_cons_mul {n : ℕ} (c u₁ : ℝ) (uₙ : tuple n)
-  : c ** (cons u₁ uₙ) = cons (c * u₁) (c ** uₙ) := begin
-  simp [tuple.scalar_mul, has_mul.mul, tuple.map],
-end
+lemma sub_cons_eq_cons_sub {n : ℕ} (u₁ v₁ : ℝ) (uₙ vₙ : tuple n)
+  : (cons u₁ uₙ) - (cons v₁ vₙ) = cons (u₁ - v₁) (uₙ - vₙ) := rfl
+
+@[simp]
+lemma mul_cons_eq_cons_mul {n : ℕ} (c head : ℝ) (tail : tuple n)
+  : c ** (cons head tail) = cons (c * head) (c ** tail) := rfl
+
+@[simp]
+lemma neg_cons_eq_cons_neg {n : ℕ} (head : ℝ) (tail : tuple n)
+  : -(cons head tail) = (cons (-head) (-tail)) := rfl
+
+@[simp]
+lemma nil_add_nil : nil + nil = nil := rfl
+
+@[simp]
+lemma zero_cons {n : ℕ} : (0 : tuple n.succ) = (cons 0 0) := rfl
+
+
+end tuple
