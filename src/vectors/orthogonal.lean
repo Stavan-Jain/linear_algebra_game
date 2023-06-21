732247ff621b2ccd582b678709008019283100cb
Stavan Jain <stavansjain@gmail.com>
2023-06-21 15:10:25 -0400

    added infix for orthogonal

--- src/vectors/orthogonal.lean
+++ src/vectors/orthogonal.lean
@@ -1,4 +1,11 @@
 import vectors.tuple
 
+@[simp]
 def orthogonal {n : ℕ} (x : tuple n) (y:tuple n) : Prop :=
 x ⬝ y = 0 
+
+infix ` ⟂ `:45 := orthogonal
+infix ` ⊥ `:45 := orthogonal
+
+#check orthogonal [[0, 0 , 0]] [[2, 3, 4]]
+#check [[0, 0 , 0]] ⊥ [[2, 3, 4]]
