2282fa5cbaae08ef6d79c12a8176158a1c5406cd
Stavan Jain <stavansjain@gmail.com>
2023-06-22 15:45:21 -0400

    added skeleton for all levels in vector_spaces_world

---
+++ src/game/vector_spaces_world/level7.lean
@@ -0,0 +1,24 @@
+import vectors.tuple -- hide
+import data.real.basic --hide
+import vectors.tuple.tactics --hide
+namespace tuple -- hide
+
+
+/- 
+
+# Vector world
+
+## Level 7: `multiplicative identity` 
+
+-/
+
+/- Lemma
+
+-/
+
+lemma one_mul : ∀ {n :ℕ } (u: tuple n), 1**u = u :=
+begin 
+  sorry, 
+end
+
+end tuple -- hide
