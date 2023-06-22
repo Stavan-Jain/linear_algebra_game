dc8acd915df81fe1e225c3bb5c530c6a21026edb
Stavan Jain <stavansjain@gmail.com>
2023-06-22 15:45:21 -0400

    added skeleton for all levels in vector_spaces_world

---
+++ src/game/vector_spaces_world/level8.lean
@@ -0,0 +1,24 @@
+import vectors.tuple -- hide
+import data.real.basic --hide
+import vectors.tuple.tactics --hide
+namespace tuple -- hide
+
+
+/- 
+
+# Vector world
+
+## Level 8: `zero_mul` 
+
+-/
+
+/- Lemma
+
+-/
+
+lemma zero_mul : ∀ {n :ℕ } (u : tuple n), 0**u = 0 :=
+begin
+  sorry, 
+end
+
+end tuple -- hide
