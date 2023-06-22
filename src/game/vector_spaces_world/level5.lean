bace6ca43abff546a095b2d1d1029a671c89b3ac
Anoushka Sinha <anoushka.sinha@duke.edu>
2023-06-22 16:07:14 -0400

    proved scalar_dist_1

--- src/game/vector_spaces_world/level5.lean
+++ src/game/vector_spaces_world/level5.lean
@@ -18,7 +18,20 @@ namespace tuple -- hide
 
 lemma scalar_dist_1 : ∀ {n : ℕ} (c : ℝ) (u v : tuple n), c** (u + v) = c** u + c** v :=
 begin 
-  sorry, 
+  intro n,
+  induction n with d hd, 
+  {intros c x y, 
+  casesm* (tuple 0), 
+  simp,
+  dsimp [scalar_mul, map],
+  refl,},
+  {intros c x y, 
+    casesm* (tuple d.succ),
+    simp,
+    split,
+    {linarith,},
+    {rw hd,},
+  },
 end
 
 end tuple -- hide
