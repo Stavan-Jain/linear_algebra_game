35f7942f6ed17abc0337957dca899df4f85dcef9
MithicSpirit <rpc01234@gmail.com>
2023-06-28 23:27:57 -0400

    Create the destruct_tuple tactic
    Effectively, for v : a ^ n for n <= m, then running
    > destruct_tuple m v using w,
    will perform cases on v a total of m times, naming each element as w_x
    where x is the appropriate index and _x is the subscript form of x. The
    final element, if any, will be called w. Omitting the "using w" will
    default to using the current name as the prefix.
    
    Proper documentation is still necessary, but this is in a working state
    now. Ideally it would be possible to make it automatically infer the
    value of m as being n, but I cannot figure out how to do that currently.

--- src/vectors/tuple/tactics.lean
+++ src/vectors/tuple/tactics.lean
@@ -2,6 +2,7 @@ import vectors.tuple
 
 namespace tuple
 
+section simp
 
 universe u
 variables {α : Type u} {n : ℕ}
@@ -41,4 +42,51 @@ lemma nnnorm_eq_sqrt_norm_sq (v : ℝ ^ n) : ‖v‖₊ = nnreal.sqrt (norm_sq v
 lemma norm_eq_sqrt_norm_sq (v : ℝ ^ n) : ‖v‖ = ↑(nnreal.sqrt (norm_sq v)) := rfl
 
 
+end simp
 end tuple
+
+
+namespace tactic
+
+open lean.parser (tk small_nat ident)
+open _root_.interactive (parse)
+open _root_.interactive.types (texpr using_ident)
+open interactive («have»)
+
+
+private meta def char.to_subscript : char → char
+| '0' := '₀'
+| '1' := '₁'
+| '2' := '₂'
+| '3' := '₃'
+| '4' := '₄'
+| '5' := '₅'
+| '6' := '₆'
+| '7' := '₇'
+| '8' := '₈'
+| '9' := '₉'
+| x := x
+
+private meta def string.to_subscript (str : string) : string := ⟨char.to_subscript <$> str.to_list⟩
+
+
+meta def destruct_tuple_core (nam : name) (curr : ℕ) : tactic unit :=
+  get_local nam >>= λ e,
+    interactive.cases_core e [`_ , ↑(to_string nam ++ string.to_subscript (to_string curr)), nam]
+
+meta def destruct_tuple_recurse (nam : name) : ℕ → ℕ → tactic unit
+| 0 curr := destruct_tuple_core nam curr
+| (n+1) curr := destruct_tuple_core nam curr >> destruct_tuple_recurse n (curr+1)
+
+meta def interactive.destruct_tuple
+  (n : parse small_nat)
+  (id : parse ident)
+  (name : parse using_ident)
+  : tactic unit := do
+    let nam := name.get_or_else id,
+    rename id nam <|> tactic.fail "Hypothesis or variable not found.",
+    destruct_tuple_recurse (name.get_or_else "v") n 1 <|>
+      tactic.fail "Could not destruct tuple. Make sure that it is of the correct type."
+
+
+end tactic
