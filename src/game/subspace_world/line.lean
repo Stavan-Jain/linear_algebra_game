ec765555cef48f52a6ae3360de8e09ec30b255f7
Anoushka Sinha <anoushka.sinha@duke.edu>
2023-06-30 16:06:49 -0400

    Proved line_through_origin

--- src/game/subspace_world/line.lean
+++ src/game/subspace_world/line.lean
@@ -1,5 +1,7 @@
 import vectors.subspace
 import game.subspace_world.rn
+import game.vector_spaces_world.vector_space
+import game.vector_world.zero_smul
 
 namespace vector_spaces
 open tuple
@@ -7,20 +9,23 @@ open set
 
 instance line_through_origin {n : ℕ} (v : ℝ ^ n): subspace (ℝ ^ n) ℝ {x : ℝ ^ n |∃(c : ℝ), x = c • v}  := begin 
   constructor, 
-  {
-    intros, simp at *,
-    cases H with c1 H1,
-    cases H_1 with c2 H2, 
-    use (c1 + c2), 
-    rw [H1, H2], sorry, 
+  { intros u h₁ w h₂,
+    simp at *,
+    cases h₁ with c₁ h₁,
+    cases h₂ with c₂ h₂, 
+    use (c₁ + c₂), 
+    rw [h₁, h₂, scalar_dist_2], } ,
 
-  } ,
-  {
-    intros, simp at *, sorry, 
-  }, 
-  {
-    intros, simp at *, sorry,  
-  },
+  { intros c₁ u h,
+    simp at *,
+    cases h with c₂ h,
+    rw h,
+    use (c₁ * c₂),
+    rw scalar_assoc, }, 
+
+  { simp,
+    use 0,
+    rw zero_smul v, /- explain ambiguous overload-/ },
 end
 
 end vector_spaces
