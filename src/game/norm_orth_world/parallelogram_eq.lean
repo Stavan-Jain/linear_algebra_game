e1c469d19eeafb8b5d34fc9ea5776a01e8b45351
Anoushka Sinha <anoushka.sinha@duke.edu>
2023-07-13 16:20:00 -0400

    changed notation for norm_sq

--- src/game/norm_orth_world/parallelogram_eq.lean
+++ src/game/norm_orth_world/parallelogram_eq.lean
@@ -20,7 +20,7 @@ As we have seen before, it may be helpful to use the fact that ||x||² can be wr
 -/
 
 lemma parallelogram_eq : ∀ {n : ℕ} (u v : ℝ ^ n), 
-(norm_sq (u + v) : ℝ) + ↑(norm_sq (u - v)) = 2 * (↑(norm_sq u) + ↑(norm_sq v)) :=
+(‖(u + v)‖² : ℝ) + ↑(‖(u - v)‖²) = 2 * (↑(‖u‖²) + ↑(‖v‖²)) :=
 begin
   intros n u v,
   rw add_norm_square,
