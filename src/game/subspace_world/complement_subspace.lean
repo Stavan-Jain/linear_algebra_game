4c4d9861626b26ad1900f28c2d77bcd665f463c7
Anoushka Sinha <anoushka.sinha@duke.edu>
2023-07-14 00:10:58 -0400

    changed notation text to make it more readable

--- src/game/subspace_world/complement_subspace.lean
+++ src/game/subspace_world/complement_subspace.lean
@@ -21,8 +21,11 @@ Think about how you can go about proving the above conditions with Lean one by o
 The split tactic will break the goal into the three conditions needed to prove that something is a subspace.  
 
 Notation:
+
 ᗮ : `\^perp`
+
 ^⟂ : `^\perp`
+
 Due to limitations of the game web editor you might need to use the ^⟂ notation. However, if you're using VSCode, both notations work.
 
 # Level 6: Prove orthogonal complement Vᗮ of subspace V is also a subspace
