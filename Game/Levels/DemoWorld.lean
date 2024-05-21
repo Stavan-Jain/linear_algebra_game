0e4889a34edad98959b0f670e7c965026be3d0fd
MithicSpirit <rpc01234@gmail.com>
2024-05-21 12:36:00 -0400

    use lean4 GameSkeleton

---
+++ Game/Levels/DemoWorld.lean
@@ -0,0 +1,8 @@
+import Game.Levels.DemoWorld.L01_HelloWorld
+
+World "DemoWorld"
+Title "Demo World"
+
+Introduction "
+This introduction is shown before one enters level 1 of the demo world. Use markdown.
+"
