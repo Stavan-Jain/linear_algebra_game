450c1718e12919457abc803c1319b31b2f02f277
MithicSpirit <rpc01234@gmail.com>
2024-05-21 12:40:40 -0400

    rebrand GameSkeleton to Linear Algebra Game

--- Game.lean
+++ Game.lean
@@ -1,26 +1,33 @@
 import Game.Levels.DemoWorld
 
 -- Here's what we'll put on the title screen
-Title "Hello World Game"
+Title "The Linear Algebra Game"
 Introduction
 "
-This text appears on the starting page where one selects the world/level to play.
-You can use markdown.
+Welcome to the linear algebra game! It works as a learning tool for Duke University’s introductory
+linear algebra course “Math 221: Linear Algebra and Applications.”
+
+It also serves to introduce you to Lean: a proof assistant that provides an environment and language
+to encode proofs formally.
+
+The linear algebra game is intended to supplement your studies in 221. While in class you will learn
+to write traditional proofs (in natural language), the game will
+
+show you how to approach such proofs formally. A formal proof is written in precise syntax and can
+be checked algorithmically by a computer program called a proof assistant.
 "
 
 Info "
-Here you can put additional information about the game. It is accessible
-from the starting through the drop-down menu.
+Developed by: Yannan Bai, Annapurna Bhattacharya, Stavan Jain, Kurt Ma, Ricardo Prado Cunha,
+Anoushka Sinha.
 
-For example: Game version, Credits, Link to Github and Zulip, etc.
-
-Use markdown.
+This game was originally developed as a part of Duke University's Math+ summer program.
 "
 
 /-! Information to be displayed on the servers landing page. -/
 Languages "English"
-CaptionShort "Game Template"
-CaptionLong "You should use this game as a template for your own game and add your own levels."
+CaptionShort "Undergraduate-level linear algebra in Lean"
+CaptionLong "TODO"
 -- Prerequisites "" -- add this if your game depends on other games
 -- CoverImage "images/cover.png"
 
